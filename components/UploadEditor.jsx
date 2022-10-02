e92fcca0deec0d7ac201a77cccc0693a6f17fb3b
kapable <skjoldenstore@gmail.com>
2022-10-02 15:22:35 +0900

    setting image with quill

--- components/UploadEditor.jsx
+++ components/UploadEditor.jsx
@@ -1,34 +1,42 @@
-import 'react-quill/dist/quill.snow.css'
-import dynamic from 'next/dynamic'
-
-
+import 'react-quill/dist/quill.snow.css';
+import dynamic from 'next/dynamic';
 const QuillNoSSRWrapper = dynamic(import('react-quill'), {
     ssr: false,
     loading: () => <p>Loading ...</p>,
 });
-import React, { useState } from 'react'
+import React, { useState, useMemo, useRef } from 'react';
 
 const UploadEditor = () => {
-    const [text, setText] = useState("");
-    const modules = {
-        toolbar: [
-            // [{ header: '1' }, { header: '2' }, { font: [] }],
-            // [{ size: [] }],
-            // ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-            // [
-            //     { list: 'ordered' },
-            //     { list: 'bullet' },
-            //     { indent: '-1' },
-            //     { indent: '+1' },
-            // ],
-            ['image'],
-            // ['clean'],
-        ],
-        clipboard: {
-          // toggle to add extra line breaks when pasting HTML:
-            matchVisual: false,
-        },
-    }
+    const [value, setValue] = useState("");
+    const quillRef = useRef();
+
+    const imageHandler = () => {
+
+    };
+    const modules = useMemo(() => {
+        return {
+            toolbar: [
+                // [{ header: '1' }, { header: '2' }, { font: [] }],
+                // [{ size: [] }],
+                // ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+                // [
+                //     { list: 'ordered' },
+                //     { list: 'bullet' },
+                //     { indent: '-1' },
+                //     { indent: '+1' },
+                // ],
+                ['image'],
+                // ['clean'],
+            ],
+            handlers: {
+                image: imageHandler,
+            },
+            clipboard: {
+              // toggle to add extra line breaks when pasting HTML:
+                matchVisual: false,
+            },
+        }
+    });
     /*
     * Quill editor formats
     * See https://quilljs.com/docs/formats/
@@ -52,10 +60,16 @@ const UploadEditor = () => {
     return (
         <div>
             <form>
-                <QuillNoSSRWrapper onChange={setText} modules={modules} formats={formats} placeholder="THIS" theme="snow" />
+                <QuillNoSSRWrapper
+                    ref={quillRef}
+                    onChange={setValue}
+                    modules={modules}
+                    formats={formats}
+                    placeholder="THIS"
+                    theme="snow" />
                 <button type='submit'>USB</button>
             </form>
-            {text}
+            {value}
         </div>
     )
 }
