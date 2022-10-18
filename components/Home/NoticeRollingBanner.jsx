14c4e3baefc396e9d6f49435ec5dfde01c5454a8
kapable <skjoldenstore@gmail.com>
2022-10-18 12:58:29 +0900

    fix rolling banner css

--- components/Home/NoticeRollingBanner.jsx
+++ components/Home/NoticeRollingBanner.jsx
@@ -1,4 +1,4 @@
-import { Row } from 'antd';
+import { Col, Row } from 'antd';
 import Link from 'next/link';
 import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
@@ -10,29 +10,31 @@ const NoticeRollingBanner = () => {
 
     return (
         <Row className='home-notice-rolling-banner'>
-            <button>공지사항</button>
-            <AliceCarousel
-                autoPlay
-                autoPlayControls={false}
-                autoPlayStrategy="none"
-                autoPlayInterval={4000}
-                animationDuration={500}
-                animationType="fadeout"
-                infinite
-                innerWidth={8}
-                touchTracking={false}
-                disableDotsControls={true}
-                disableButtonsControls={true}
-                items={
-                    categoryNew15Posts
-                    ? (categoryNew15Posts.map((post) => (
-                        <Link href={`notice/${post?.SubCategory?.domain}/${post.id}`}><a>
-                            <p>{post.title}</p>
-                        </a></Link>
-                    )))
-                    : (<p>현재 공지글이 존재하지 않습니다.</p>)
-                }
-            />
+            <Col xs={{ span: 6 }} lg={{ span: 2 }} span={6}><button>공지사항</button></Col>
+            <Col xs={{ span: 18 }} lg={{ span: 22 }} span={18} >
+                <AliceCarousel
+                    autoPlay
+                    autoPlayControls={false}
+                    autoPlayStrategy="none"
+                    autoPlayInterval={4000}
+                    animationDuration={500}
+                    animationType="fadeout"
+                    infinite
+                    innerWidth={8}
+                    touchTracking={false}
+                    disableDotsControls={true}
+                    disableButtonsControls={true}
+                    items={
+                        categoryNew15Posts
+                        ? (categoryNew15Posts.map((post) => (
+                            <Link href={`notice/${post?.SubCategory?.domain}/${post.id}`}><a>
+                                <p>{post.title}</p>
+                            </a></Link>
+                        )))
+                        : (<p>현재 공지글이 존재하지 않습니다.</p>)
+                    }
+                />
+            </Col>
         </Row>
     );
 };
